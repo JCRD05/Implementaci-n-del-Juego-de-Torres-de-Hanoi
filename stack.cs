39638218387df3f300a9d9b87aaeba36053f9633
JCRD05 <josecarlosruizdiaz05@gmail.com>
2025-02-20 16:58:05 -0600

    Update stack.cs

--- stack.cs
+++ stack.cs
@@ -10,10 +10,9 @@ namespace Data.Structure.Stack
     class Stack
     {
         public Node head; // Primer elemento del stack o top
-        private string name; // Nombre del stack
-        private int length; // Variable que almacena la cantidad de elementos del stack
+        private string name; 
+        public int length; // Variable que almacena la cantidad de elementos del stack
         
-        // Constructor de la clase Stack
         // Crea la estructura sin ningun dato almacenado
         public Stack(string name) 
         {
@@ -25,30 +24,29 @@ namespace Data.Structure.Stack
         // Metodo que agrega un elemento al principio del stack
         public void Push(int data)
         {
-            // Si esta vacio crea un nuevo nodo y almacena el elemento
-            if(IsEmpty()){ head = new Node(data); } 
+            if(IsEmpty()) { head = new Node(data); } 
             else { head = new Node(data, head); }
             length++;
         }
         
-        // Metodo que elimina un elemento del principio del stack
+        // Metodo que elimina un elemento del principio del stack y lo retorna
         public int Pop()
         {
-            if(IsEmpty()){ return 0; } // Si el stack esta vacio retorna la funcion
+            if(IsEmpty()) { return 0; } 
             
-            int erasedData = head.data; // Almacena el dato del elemento eliminado
+            int erasedData = head.data; 
             
-            if(length == 1){ head = null; }
+            if(length == 1) { head = null; }
             else { head = head.next; }
             length--;
             
-            return erasedData; // Retorna el elemento eliminado
+            return erasedData; 
         }
         
         // Metodo que retorna el elemento que se encuentra al principio del stack
         public int Peek() 
         {
-            if (IsEmpty()){ return 0; } // Si el stack esta vacio retorna 0
+            if (IsEmpty()) { return 0; } 
             else { return head.data; } 
         }
         
@@ -64,20 +62,25 @@ namespace Data.Structure.Stack
             else 
             {
                 Node current = head;
-                Console.WriteLine("\n" + name + "\n");
+                int[] stackData = new int[length];
                 for(int i = 0; i < length; i++)
                 {
-                    Console.WriteLine(current.data);
+                    stackData[i] = current.data;
                     current = current.next;
                 } 
-                Console.WriteLine("\n");
+                
+                Console.WriteLine("\n" + name + "\n");
+                for(int i = length; i > 0; i--)
+                {
+                    Console.Write(stackData[i - 1] + " ");
+                } 
             }
         }
         
         // Metodo que checa si la pila se encuentra vacia
         public bool IsEmpty() 
         {
-            if (length == 0){ return true; }
+            if (length == 0) { return true; }
             else { return false; }
         }
     }
